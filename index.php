eaaa29b178af9937042a2e2e721f71ff154885c3
matsufuji06 <n.matsufuji06@gmail.com>
2021-11-16 21:31:24 +0900

    PDOでMySQLに接続

--- index.php
+++ index.php
@@ -18,7 +18,13 @@
 <main>
 <h2>Practice</h2>
 <pre>
-
+  <?php
+    try {
+      $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8','root','root');
+    } catch(PDOException $e) {
+      echo 'DB接続エラー：' . $e->getMessage();
+    }
+  ?>
 </pre>
 </main>
 </body>    
