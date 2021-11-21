c1b0df1e14a93af603774b7db319f01079ce8696
matsufuji06 <n.matsufuji06@gmail.com>
2021-11-21 19:46:40 +0900

    memosのデータをfetchで一覧表示（※接続できない）

--- index.php
+++ index.php
@@ -17,13 +17,14 @@
 
 <main>
 <h2>Practice</h2>
-<pre>
   <?php
     try {
       $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8','root','root');
     } catch(PDOException $e) {
       echo 'DB接続エラー：' . $e->getMessage();
     }
+
+    $memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
 
     // $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰, ピンク, 甘い"');
     // echo $count . '件のデータを挿入しました。';
@@ -35,7 +36,14 @@
 
     
   ?>
-</pre>
+
+  <article>
+    <?php while ($memo = $memos->fetch()); ?>
+      <p><a href="#"><?php print($memo['memo']); ?></a></p>
+      <time><?php print($memo['created_at']); ?></time>
+      <hr>
+    <?php endwhile; ?>
+  </article>
 </main>
 </body>    
 </html>
